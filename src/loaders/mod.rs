a069e932ab000d38076cb9a5611e94083f326780
LempekPL <superlempek2004@gmail.com>
2022-04-17 15:11:04 +0200

    display window in the middle of screen

--- src/loaders/mod.rs
+++ src/loaders/mod.rs
@@ -1,14 +1,14 @@
 mod preload;
 mod setup;
 mod setup_settings;
-mod setup_icon;
+mod setup_window;
 
 use bevy::prelude::*;
 use crate::AppState;
 use self::setup::{setup, setup_sound};
 use self::preload::preload;
 use self::setup_settings::setup_settings;
-use self::setup_icon::setup_icon;
+use self::setup_window::setup_window;
 
 pub struct LoaderPlugin;
 
@@ -16,7 +16,7 @@ impl Plugin for LoaderPlugin {
     fn build(&self, app: &mut App) {
         // forcing preload before anything else
         app.add_state(AppState::Preload);
-        app.add_startup_system(setup_icon);
+        app.add_startup_system(setup_window);
         app.add_startup_system(preload);
         // initiating setup after asset loading
         app.add_system_set(SystemSet::on_enter(AppState::Loading(0))
--- src/loaders/setup_window.rs
+++ src/loaders/setup_window.rs
@@ -3,8 +3,9 @@ use bevy::window::WindowId;
 use bevy::winit::WinitWindows;
 use winit::window::Icon;
 
-pub fn setup_icon(
+pub fn setup_window(
     windows: NonSend<WinitWindows>,
+    mut window: ResMut<Windows>,
 ) {
     let primary = windows.get_window(WindowId::primary()).unwrap();
     let (icon_rgba, icon_width, icon_height) = {
@@ -17,4 +18,10 @@ pub fn setup_icon(
     };
     let icon = Icon::from_rgba(icon_rgba, icon_width, icon_height).unwrap();
     primary.set_window_icon(Some(icon));
+    let monitor = primary.primary_monitor().unwrap();
+    // set window position to be in the middle of primary screen
+    let primary_game = window.get_primary_mut().unwrap();
+    let w_pos = ((monitor.size().width as f32 - primary_game.width()) / 2.0) as i32;
+    let h_pos = ((monitor.size().height as f32 - primary_game.height()) / 2.0) as i32;
+    primary_game.set_position(IVec2::new(w_pos, h_pos));
 }
