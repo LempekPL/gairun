bc86446aa92bfab3be27a743ba9c948f0c54f242
LempekPL <superlempek2004@gmail.com>
2022-04-24 17:28:59 +0200

    camera follow

--- src/camera.rs
+++ src/camera.rs
@@ -1,9 +1,12 @@
 use bevy::prelude::*;
-use crate::{AppState, Camera2D};
+use bevy::render::camera::Camera2d;
+use crate::AppState;
 use crate::entity::Player;
 use crate::InGameState::Playing;
 
 pub(crate) struct CameraPlugin;
+
+const BOX_SIZE: f32 = 30.0;
 
 impl Plugin for CameraPlugin {
     fn build(&self, app: &mut App) {
@@ -14,15 +17,27 @@ impl Plugin for CameraPlugin {
     }
 }
 
+#[warn(clippy::type_complexity)]
 fn camera_follow_player(
     mut p_cam_player: ParamSet<(Query<&mut Transform, With<Camera2d>>, Query<&Transform, With<Player>>)>
 ) {
     let player = p_cam_player.p1();
     let player = player.single();
-    let x = player.translation.x;
-    let y = player.translation.y;
+    let (p_x, p_y) = (player.translation.x, player.translation.y);
     let mut q_cam = p_cam_player.p0();
     let mut cam = q_cam.single_mut();
-    cam.translation.x = x;
-    cam.translation.y = y;
+    let (mut c_x, mut c_y) = (cam.translation.x, cam.translation.y);
+
+    if c_x - BOX_SIZE > p_x {
+        c_x += (p_x - c_x + BOX_SIZE) / BOX_SIZE;
+    } else if c_x + BOX_SIZE < p_x {
+        c_x += (p_x - c_x - BOX_SIZE) / BOX_SIZE;
+    }
+    if c_y - BOX_SIZE > p_y {
+        c_y += (p_y - c_y + BOX_SIZE) / BOX_SIZE;
+    } else if c_y + BOX_SIZE < p_y {
+        c_y += (p_y - c_y - BOX_SIZE) / BOX_SIZE;
+    }
+    cam.translation.x = c_x;
+    cam.translation.y = c_y;
 }
