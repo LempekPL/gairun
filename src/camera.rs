2a213d893dacf1175ca4aa308cbc69bf656acc19
LempekPL <superlempek2004@gmail.com>
2022-05-04 00:10:00 +0200

    camera fix: c_., p_. better explained; better speed control; cleanup

--- src/camera.rs
+++ src/camera.rs
@@ -6,7 +6,8 @@ use crate::InGameState::Playing;
 
 pub(crate) struct CameraPlugin;
 
-const BOX_SIZE: f32 = 30.0;
+const BOX_SIZE: f32 = 64.0;
+const CAMERA_SPEED: f32 = 0.064;
 
 impl Plugin for CameraPlugin {
     fn build(&self, app: &mut App) {
@@ -19,25 +20,33 @@ impl Plugin for CameraPlugin {
 
 #[warn(clippy::type_complexity)]
 fn camera_follow_player(
-    mut p_cam_player: ParamSet<(Query<&mut Transform, With<Camera2d>>, Query<&Transform, With<Player>>)>
+    mut q_camera_player: ParamSet<(Query<&mut Transform, With<Camera2d>>, Query<&Transform, With<Player>>)>
 ) {
-    let player = p_cam_player.p1();
-    let player = player.single();
-    let (p_x, p_y) = (player.translation.x, player.translation.y);
-    let mut q_cam = p_cam_player.p0();
-    let mut cam = q_cam.single_mut();
-    let (mut c_x, mut c_y) = (cam.translation.x, cam.translation.y);
+    let player = q_camera_player.p1();
+    let player = player.get_single();
+    if let Ok(player) = player {
+        let (player_x, player_y) = (player.translation.x, player.translation.y);
+        let mut q_camera = q_camera_player.p0();
+        let mut camera = q_camera.single_mut();
 
-    if c_x - BOX_SIZE > p_x {
-        c_x += (p_x - c_x + BOX_SIZE) / BOX_SIZE;
-    } else if c_x + BOX_SIZE < p_x {
-        c_x += (p_x - c_x - BOX_SIZE) / BOX_SIZE;
+        camera.translation.x += move_camera(camera.translation.x, player_x);
+        camera.translation.y += move_camera(camera.translation.y, player_y);
+    } else {
+        let mut q_camera = q_camera_player.p0();
+        let mut camera = q_camera.single_mut();
+
+        camera.translation.x += move_camera(camera.translation.x, 0.);
+        camera.translation.y += move_camera(camera.translation.y, 0.);
     }
-    if c_y - BOX_SIZE > p_y {
-        c_y += (p_y - c_y + BOX_SIZE) / BOX_SIZE;
-    } else if c_y + BOX_SIZE < p_y {
-        c_y += (p_y - c_y - BOX_SIZE) / BOX_SIZE;
+}
+
+// helper function
+fn move_camera(camera: f32, player: f32) -> f32 {
+    if camera - BOX_SIZE > player {
+        return (player - camera + BOX_SIZE) * CAMERA_SPEED;
     }
-    cam.translation.x = c_x;
-    cam.translation.y = c_y;
+    if camera + BOX_SIZE < player {
+        return (player - camera - BOX_SIZE) * CAMERA_SPEED;
+    }
+    return 0.;
 }
