947e33ee0e4ef85f3b0751e21a167ecd5485c1d1
LempekPL <superlempek2004@gmail.com>
2022-05-05 01:17:51 +0200

    presence fix: for some reason changing values in presence stops entire app for some seconds even optimized; fix: set and never change :/

--- src/main.rs
+++ src/main.rs
@@ -9,11 +9,13 @@ mod mapper;
 mod toasts;
 mod main_menu;
 mod menu;
-mod presence;
 
 use bevy::prelude::*;
+use bevy_discord_presence::config::{RPCConfig, RPCPlugin};
+use bevy_discord_presence::state::ActivityState;
 use bevy_inspector_egui::WorldInspectorPlugin;
 use bevy_kira_audio::AudioPlugin;
+use discord_presence::models::ActivityAssets;
 use crate::asset_loader::AssetLoaderPlugin;
 use crate::camera::CameraPlugin;
 use crate::entity::EntityPlugin;
@@ -22,13 +24,12 @@ use crate::mapper::MapPlugin;
 use crate::toasts::ToastsPlugin;
 use crate::main_menu::MainMenuPlugin;
 use crate::menu::InGameMenuPlugin;
-use crate::presence::DiscordPlugin;
 
 fn main() {
     let mut app = App::new();
     app.insert_resource(ClearColor(Color::BLACK));
     app.insert_resource(WindowDescriptor {
-        title: "Gairun".to_string(),
+        title: "gairun".to_string(),
         resizable: false,
         width: 1920.0,
         height: 1080.0,
@@ -37,6 +38,17 @@ fn main() {
     app.add_plugins(DefaultPlugins);
     app.add_plugin(AudioPlugin);
     app.add_plugin(WorldInspectorPlugin::new());
+    // only use discordRPC if compiled with --release flag
+    // unoptimized compiled version slows down the app
+    if !cfg!(debug_assertions) {
+        app.add_plugin(RPCPlugin(
+            RPCConfig {
+                app_id: 971525507541790720,
+                show_time: true,
+            }
+        ));
+        app.add_startup_system(update_presence);
+    }
 
     app.add_plugin(AssetLoaderPlugin);
     app.add_plugin(MainMenuPlugin);
@@ -46,11 +58,6 @@ fn main() {
     app.add_plugin(EntityPlugin);
     app.add_plugin(CameraPlugin);
     app.add_plugin(MapPlugin);
-    // only use discordRPC if compiled with --release flag
-    // unoptimized compiled version slows down the app
-    if !cfg!(debug_assertions) {
-        app.add_plugin(DiscordPlugin);
-    }
 
     app.insert_resource(GlobalScale(Vec3::new(2.0, 2.0, 2.0)));
 
@@ -82,4 +89,15 @@ pub enum MainMenus {
 pub enum InGameState {
     Playing,
     Paused,
+}
+
+fn update_presence(mut state: ResMut<ActivityState>) {
+    // add discord presence
+    state.details = Some("Playing".to_string());
+    state.assets = Some(ActivityAssets {
+        large_image: Some("icon".to_string()),
+        large_text: Some("gairun".to_string()),
+        small_image: None,
+        small_text: None,
+    });
 }
