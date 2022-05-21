e3d877c580d27a207c45febb7e915ee6f3336e01
LempekPL <superlempek2004@gmail.com>
2022-05-21 17:31:45 +0200

    🔧 setup_icon fix: display error ingame instead of panicking

--- src/loaders/setup_icon.rs
+++ src/loaders/setup_icon.rs
@@ -3,15 +3,26 @@ use bevy::window::WindowId;
 use bevy::winit::WinitWindows;
 use winit::window::Icon;
 use winit::platform::windows::WindowExtWindows;
+use crate::toasts::ToastEvent;
 
 pub fn setup_icon(
     windows: NonSend<WinitWindows>,
+    mut ev_toast: EventWriter<ToastEvent>,
 ) {
     let primary = windows.get_window(WindowId::primary()).unwrap();
     let (icon_rgba, icon_width, icon_height) = {
-        let image = image::open("icon.png")
-            .expect("Failed to open icon path")
-            .into_rgba8();
+        let image = match image::open("assets/resources/icon.png") {
+            Ok(image) => { image.into_rgba8() }
+            Err(_) => {
+                ev_toast.send(ToastEvent {
+                    text: "Couldn't load icon".to_string(),
+                    text_color: Color::WHITE,
+                    background_color: Color::ORANGE,
+                    font: None,
+                });
+                return;
+            }
+        };
         let (width, height) = image.dimensions();
         let rgba = image.into_raw();
         (rgba, width, height)
